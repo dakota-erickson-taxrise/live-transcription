c2582bb147469c7c00f9fe88f45c56c294e897e3
Dakota Erickson <derickson@taxrise.com>
2024-12-20 11:06:13 -0600

    find out what the websocket message looks like

--- main.py
+++ main.py
@@ -98,6 +98,7 @@ class TranscriptionWebSocket:
     async def handle_websocket(self, websocket: websockets.WebSocketServerProtocol):
         self.websocket = websocket
         logging.info(f"Client connected from {websocket.remote_address}")
+        logging.info(f"websocket is ${json.dumps(websocket)}")
 
         def on_transcription_data(transcript: aai.RealtimeTranscript):
             if not transcript.text:
